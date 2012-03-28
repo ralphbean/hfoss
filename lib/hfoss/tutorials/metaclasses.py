89a33e0d5f107d8849ceea3d3f281b779d45cc97
Ralph Bean <rbean@redhat.com>
2012-03-28 09:33:52 -0400

    Metaclasses example.

---
+++ lib/hfoss/tutorials/metaclasses.py
@@ -0,0 +1,45 @@
+from random import random
+
+class SwitchboardMeta(type):
+    def __new__(meta, name, bases, dct):
+        if bases[-1] == object:
+            bases = bases[:-1]
+
+        bases += pick_random_parents()
+
+        if not bases:
+            bases = (object,)
+
+        return type.__new__(meta, name, bases, dct)
+
+class GrandParent(object):
+    def awesome(self):
+        print "I'm the grandparent."
+
+class Parent1(GrandParent):
+    def awesome(self):
+        print "I'm parent1"
+        super(Parent1, self).awesome()
+
+class Parent2(GrandParent):
+    def awesome(self):
+        print "I'm parent2"
+        super(Parent2, self).awesome()
+
+class Parent3(GrandParent):
+    def awesome(self):
+        print "I'm parent3"
+        super(Parent3, self).awesome()
+
+def pick_random_parents():
+    return tuple((p for p in [Parent1, Parent2, Parent3] if random() < 0.5))
+
+class Child(object):
+    __metaclass__ = SwitchboardMeta
+    def awesome(self):
+        print "I'm the child."
+        super(Child, self).awesome()
+
+if __name__ == '__main__':
+    child = Child()
+    child.awesome()
