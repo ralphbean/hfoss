1e1423cbe3556069b518c84a12f3929b56c0be89
Ralph Bean <rbean@redhat.com>
2012-03-28 08:27:59 -0400

    Added inspect tutorial.

---
+++ lib/hfoss/tutorials/inspect_module.py
@@ -0,0 +1,37 @@
+""" This is the module level docstring. """
+
+import inspect
+import sys
+
+
+def my_doc(n=1):
+    """ Print the docstring of the calling function """
+    frame = inspect.stack()[n][0]
+    return frame.f_globals[frame.f_code.co_name].__doc__
+
+
+def f(x=None):
+    """ This is function f.  It has a docstring.
+
+    It is many lines long, and might provide examples.
+    """
+
+    # Validate the input
+    if x == None:
+        print my_doc()
+        print "You must provide a value for 'x'."
+        return ""
+
+    # Otherwise, do the work we intended to do.
+    return "%r is your value!" % x
+
+
+if __name__ == '__main__':
+    print __doc__   # Print the module level docstring
+    print "-" * 50  # Separate our output
+    print "About to run function f:"
+    print inspect.getsource(f)
+    print "-" * 50  # Separate our output
+    print f()       # Run it with a broken input.
+    print "-" * 50  # Separate our output
+    print f()       # Run it with good input.
